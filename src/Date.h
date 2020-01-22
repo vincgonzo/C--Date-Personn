588bd4b8c303eaa6383cdd473fe686f57bd50a9a
vincent-castanie <vincgonzo@hotmail.fr>
2020-01-22 13:14:43 +0100

    update Date.h

--- src/Date.h
+++ src/Date.h
@@ -5,18 +5,26 @@
 #include <vector>
 #include <iostream>
 
+struct DateInvalide {
+    DateInvalide() {
+        std::cout << "Erreur";
+    }
+};
+
 class Date {
 
     public:
+        Date();
         Date(int m_day, int m_month, int m_year);
         void prettyPrint();
         void increment();
-        int dayOfYear();
-        int dayInMonth();
+        int dayOfYear() const;
+        int dayInMonth(unsigned int m, unsigned int y );
         void setMonth(unsigned int m);
         void setDay(unsigned int d);
         void setYear(unsigned int y);
-        bool isBisextile();
+        bool isBisextile(unsigned int y);
+        bool validDate(unsigned int day, unsigned int month, unsigned int year);
 
         std::string toString();
         std::string getCurrentMonth();
@@ -24,7 +32,6 @@ class Date {
     private:
         int m_day, m_month, m_year;
         std::vector <std::string> m_month_name = {"janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"};
-
 };
 
 #endif // DATE_H_INCLUDED
